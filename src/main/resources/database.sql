70e833eeba85ea279eea1593cf2773e8b69104dd
Alexey Vedensky <avedensky@gmail.com>
2017-04-26 19:54:24 +0300

    Update database.sql

---
+++ src/main/resources/database.sql
@@ -0,0 +1,21 @@
+# Create DataBase and add user
+# CREATE USER todouser@localhost identified BY '1234';
+# GRANT usage ON *.* TO todouser@localhost identified BY '1234';
+# CREATE DATABASE IF NOT EXISTS todo_list CHARACTER SET utf8;
+# GRANT ALL privileges ON todo_list.* TO todouser@localhost; USE todo_list;
+
+
+# Create table
+CREATE TABLE IF NOT EXISTS todo_list.tasks (
+  id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  create_dt   DATETIME,
+  description VARCHAR(79),
+  hasDone     BIT NULL     DEFAULT 0
+)
+  ENGINE = InnoDB;
+
+# Fill data
+INSERT INTO todo_list.tasks VALUES (1, 20170426161122, 'Завести репозиторий на GIT', 1);
+INSERT INTO todo_list.tasks VALUES (2, 20170426171537, 'Создать проект на Maven', 1);
+INSERT INTO todo_list.tasks VALUES (3, 20170426182121, 'Запустить и проверить что все работает', 1);
+INSERT INTO todo_list.tasks VALUES (4, 20170426194152, 'Добавить gitignore  файл в проектную директорию', 1);
