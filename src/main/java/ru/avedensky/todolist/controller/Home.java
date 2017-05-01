c04ef3093a39fa6145cf433bf68d6b8b8aad442f
Alexey Vedensky <avedensky@gmail.com>
2017-05-01 16:01:58 +0300

    Change package name

--- src/main/java/ru/avedensky/todolist/controller/Home.java
+++ src/main/java/ru/avedensky/todolist/controller/Home.java
@@ -1,4 +1,4 @@
-package ru.avedensky.todolist;
+package ru.avedensky.todolist.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
